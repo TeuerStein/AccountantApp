53323a49bf7e0dd1b2ab3ae73e7d78173442dc1f
TeuerStein <xenkai123456789@gmail.com>
2020-11-06 12:07:16 +0100

    Initial commit

---
+++ AccountantApp/AccountantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountantAppApp.swift
+//  AccountantApp
+//
+//  Created by ALEXANDR DRAGUNOV on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct AccountantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
